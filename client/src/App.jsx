5f72747cd81c06db6794aeb858248ea8d41d04f9
I.Chandana <72302495+Indu-Chandana@users.noreply.github.com>
2021-09-25 19:53:53 +0530

    001

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+import './App.css'; 
+
+import { ChannelListContainer, ChannelContainer, Auth } from './components'; //import files from components
+
+const apiKey = '8csn54b8uf4c';
+
+const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
+const App = () => {
+
+    if(!authToken) return <Auth/>
+    
+    return (
+        <div className="app__wrapper">
+            <Chat client={client} theme="team light">
+                <ChannelListContainer/>
+                <ChannelContainer/>
+            </Chat>
+        </div>
+    )
+}
+
+export default App
